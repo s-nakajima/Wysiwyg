def44a2c3f2d2d9d67d2eee9c9200113e0b372c2
Satoru Majima <satoru@majima.net>
2016-03-01 18:51:15 +0900

    ファイルアップロード処理の実装
    /wysiwyg/file/upload によるアップロードの実行対応
    
    * Fileコントローラー
    * Uploadアクション
    
    を作成し、FileUploadコンポーネントと UploadFileモデルを利用したファイル登録処理を行う対応。
    
    戻り値は UploadFileモデルの返すファイル情報と
    送信したファイルの POST変数を jsonで返すように一時的に設定

---
+++ Controller/FileController.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * File Controller
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Satoru Majima <neo.otokomae@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('WysiwygAppController', 'Wysiwyg.Controller');
+
+/**
+ * File Controller
+ *
+ * @author Satoru Majima <neo.otokomae@gmail.com>
+ * @package NetCommons\Wysiwyg\Controller
+ */
+class FileController extends WysiwygAppController {
+
+/**
+ * use component
+ *
+ * @var array
+ */
+	public $components = array(
+		'Security',
+		'Files.FileUpload',
+		'Files.Download',
+	);
+
+/**
+ * beforeFilter
+ *
+ * @return void
+ */
+	public function beforeFilter() {
+		parent::beforeFilter();
+
+		// TinyMCE のアップロードフォームに Token埋め込み方法が未解決のため
+		// ひとまずセキュリティ Component から uploadアクションを除外する
+		$this->Security->unlockedActions = array('upload', 'download');
+	}
+
+/**
+ * upload action
+ *
+ * file: data[Wysiwyg][file]
+ * block_key: data[Block][key]
+ * としてそれぞれ POSTされるものとして作成。
+ *
+ * @return void
+ */
+	public function upload() {
+		// 初期処理
+		$uploadFileModel = ClassRegistry::init('Files.UploadFile');
+
+		// FileUploadコンポーネントからアップロードファイル情報の取得
+		$file = $this->FileUpload->getTemporaryUploadFile('Wysiwyg.file');
+
+		// uploadFile登録に必要な data（block_key）を作成する。
+		$data = [
+			'UploadFile' => [
+				'block_key' => $this->data['Block']['key']
+			]
+		];
+		$uploadFile = $uploadFileModel->registByFile($file, 'wysiwyg', null, 'Wysiwyg.file', $data);
+
+		$requestFile = $this->data['Wysiwyg']['file'];
+
+		// 戻りとして JSONを返す
+		$this->viewClass = 'Json';
+		$this->set(compact('uploadFile', 'requestFile'));
+		$this->set('_serialize', ['uploadFile', 'requestFile']);
+	}
+}
