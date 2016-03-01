3c70925064ca6518e920b942d6ce834a71452463
Satoru Majima <satoru@majima.net>
2016-03-01 20:53:46 +0900

    FileおよびImageコントローラーの作成
    諸々の製作を進めやすくするために空の Controllerファイルを作成対応。

---
+++ Controller/FileController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * File Controller
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Satoru Majima <neo.otokomae@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('WysiwygAppController', 'Wysiwyg.Controller');
+
+/**
+ * File Controller
+ *
+ * @author Satoru Majima <neo.otokomae@gmail.com>
+ * @package NetCommons\Wysiwyg\Controller
+ */
+class FileController extends WysiwygAppController {
+
+}
