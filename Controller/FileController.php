13e43718ba8b68f593ef7a048c8b4479d35c6a7b
Satoru Majima <satoru@majima.net>
2016-03-01 23:52:32 +0900

    FileController を作った状態の更新とのコンフリクトの解消

--- Controller/FileController.php
+++ Controller/FileController.php
@@ -19,4 +19,59 @@ App::uses('WysiwygAppController', 'Wysiwyg.Controller');
  */
 class FileController extends WysiwygAppController {
 
+/**
+ * use component
+ *
+ * @var array
+ */
+	public $components = array(
+		'Security',
+		'Files.FileUpload',
+		'Files.Download',
+	);
+
+/**
+ * beforeFilter
+ *
+ * @return void
+ */
+	public function beforeFilter() {
+		parent::beforeFilter();
+
+		// TinyMCE のアップロードフォームに Token埋め込み方法が未解決のため
+		// ひとまずセキュリティ Component から uploadアクションを除外する
+		$this->Security->unlockedActions = array('upload', 'download');
+	}
+
+/**
+ * upload action
+ *
+ * file: data[Wysiwyg][file]
+ * block_key: data[Block][key]
+ * としてそれぞれ POSTされるものとして作成。
+ *
+ * @return void
+ */
+	public function upload() {
+		// 初期処理
+		$uploadFileModel = ClassRegistry::init('Files.UploadFile');
+
+		// FileUploadコンポーネントからアップロードファイル情報の取得
+		$file = $this->FileUpload->getTemporaryUploadFile('Wysiwyg.file');
+
+		// uploadFile登録に必要な data（block_key）を作成する。
+		$data = [
+			'UploadFile' => [
+				'block_key' => $this->data['Block']['key']
+			]
+		];
+		$uploadFile = $uploadFileModel->registByFile($file, 'wysiwyg', null, 'Wysiwyg.file', $data);
+
+		$requestFile = $this->data['Wysiwyg']['file'];
+
+		// 戻りとして JSONを返す
+		$this->viewClass = 'Json';
+		$this->set(compact('uploadFile', 'requestFile'));
+		$this->set('_serialize', ['uploadFile', 'requestFile']);
+	}
 }
