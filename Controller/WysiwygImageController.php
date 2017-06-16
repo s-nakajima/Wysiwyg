3eb99a407a8f119623e840086cb3a1e86b25cdf9
kteraguchi <kteraguchi@commonsnet.org>
2017-06-16 20:01:44 +0900

    Update for performance
    NetCommons3/NetCommons3#917
    この処理でいけそうな気がするがどうだろう。

--- Controller/WysiwygImageController.php
+++ Controller/WysiwygImageController.php
@@ -10,6 +10,7 @@
  */
 
 App::uses('WysiwygFileController', 'Wysiwyg.Controller');
+App::uses('AppModel', 'Model');
 
 /**
  * Image Controller
@@ -30,6 +31,101 @@ class WysiwygImageController extends WysiwygFileController {
 			'message' => 'File is not a image'
 		]
 	];
+
+/**
+ * Loads Model classes based on the uses property
+ * see Controller::loadModel(); for more info.
+ * Loads Components and prepares them for initialization.
+ *
+ * @return mixed true if models found and instance created.
+ * @see Controller::loadModel()
+ * @link http://book.cakephp.org/2.0/en/controllers.html#Controller::constructClasses
+ * @throws MissingModelException
+ */
+	public function constructClasses() {
+		//$this->components['NetCommons.AccessCtrl']['enabled'] = false;
+		//$this->components['Auth']['enabled'] = false;
+		//$this->components['Flash']['enabled'] = false;
+		//$this->components['MobileDetect.MobileDetect']['enabled'] = false;
+		//$this->components['NetCommons.Asset']['enabled'] = false;
+		$this->components['NetCommons.Permission']['enabled'] = false;
+		$this->components['NetCommons.NetCommons']['enabled'] = false;
+		$this->components['NetCommons.NetCommonsTime']['enabled'] = false;
+		$this->components['RequestHandler']['enabled'] = false;
+		$this->components['Session']['enabled'] = false;
+		$this->components['Workflow.Workflow']['enabled'] = false;
+		$this->components['Security']['enabled'] = false;
+		//$this->components['Files.FileUpload']['enabled'] = false;
+		//$this->components['Files.Download']['enabled'] = false;
+		$this->components['Wysiwyg.Wysiwyg']['enabled'] = false;
+
+		return parent::constructClasses();
+	}
+
+/**
+ * Called before the controller action. You can use this method to configure and customize components
+ * or perform logic that needs to happen before each controller action.
+ *
+ * @return void
+ * @link http://book.cakephp.org/2.0/en/controllers.html#request-life-cycle-callbacks
+ */
+	public function beforeFilter() {
+		/* @var $Room AppModel */
+		// シンプルにしたかったためAppModelを利用。インスタンス生成時少し速かった。
+		$settings = [
+			'table' => 'rooms',
+			'alias' => 'Room',
+		];
+		$Room = new AppModel($settings);
+		$params = [
+			'belongsTo' => [
+				'TrackableCreator',
+				'TrackableUpdater',
+			]
+		];
+		$Room->unbindModel($params);
+		$Room->Behaviors->unload('Trackable');
+
+		$params = [
+			'hasOne' => [
+				'Space' => [
+					'className' => 'Space',
+					'foreignKey' => false,
+					'conditions' => [
+						'Space.id = Room.space_id',
+					],
+					'fields' => ['type']
+				],
+				'RolesRoomsUser' => [
+					'className' => 'RolesRoomsUser',
+					'conditions' => [
+						'RolesRoomsUser.user_id' => AuthComponent::user('id'),
+					],
+					'fields' => ['id']
+				],
+			],
+		];
+		$Room->bindModel($params);
+
+		$query = [
+			'conditions' => [
+				'Room.id' => $this->request->params['pass'][0]
+			],
+			'recursive' => 0,
+			'callbacks' => false,
+		];
+		$room = $Room->find('first', $query);
+		if (!$room) {
+			return;
+		}
+
+		App::uses('Space', 'Rooms.Model');
+		if ($room['Space']['type'] === Space::PUBLIC_SPACE_ID ||
+			isset($room['RolesRoomsUser']['id'])
+		) {
+			Current::setCurrent($room);
+		}
+	}
 
 /**
  * download action
